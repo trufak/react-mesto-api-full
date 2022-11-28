afc0fa8a6c17a739368ec90f4cd733296efe8a8c
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-11-28 11:38:51 +0700

    fix: изменить cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,3 @@
-// Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
   'http://localhost:3000',
   'https://mesto.trufakin.nomoredomains.club',
@@ -8,7 +7,8 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { origin, method } = req.headers;
+  const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
