f45b9cdd3cad114e32a4263e9d2263ea4899d74b
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-11-28 09:42:36 +0700

    fix: исправить ошибки cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,14 +10,14 @@ module.exports = (req, res, next) => {
   const { origin, method } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
   }
 
   return next();
