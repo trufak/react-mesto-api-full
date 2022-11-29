94902110df5c828bcd21f58e5033290105d1e719
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-11-29 09:03:11 +0700

    fix: исправить подключение к БД

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose
-  .connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(cors);
 
