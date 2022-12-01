c588db68d2b142f518fb6b3afa8957cd8a9874f4
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-12-01 09:03:20 +0700

    feat: подключение helmet

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors: errorsCelebrate } = require('celebrate');
+const helmet = require('helmet');
+
 const errors = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 const router = require('./routes/index');
+const limiter = require('./middlewares/limiter');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-
+/* подключение helmet */
+app.use(helmet());
+/* ограничение количества запросов */
+app.use(limiter);
+/* выставление заголовков cors */
 app.use(cors);
-
+/* парсинг тела запроса */
 app.use(bodyParser.json());
 /* логирование запросов */
 app.use(requestLogger);
